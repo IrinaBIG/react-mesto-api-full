f8ecf3a02c5f92942df0b7746422a3713bf33750
Irina Berestova <berestovaecdep@mail.ru>
2022-11-02 01:11:18 +0700

    JWT_SECRET_KEY2

--- backend/test.js
+++ backend/test.js
@@ -5,17 +5,20 @@ const SECRET_KEY_DEV = 'dev-secret'; // вставьте сюда секретн
 try {
   const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
 
+  // eslint-disable-next-line no-console
   console.log('\x1b[31m%s\x1b[0m', `
 Надо исправить. В продакшне используется тот же
 секретный ключ, что и в режиме разработки.
 `);
 } catch (err) {
   if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+    // eslint-disable-next-line no-console
     console.log(
       '\x1b[32m%s\x1b[0m',
       'Всё в порядке. Секретные ключи отличаются',
     );
   } else {
+    // eslint-disable-next-line no-console
     console.log(
       '\x1b[33m%s\x1b[0m',
       'Что-то не так',
